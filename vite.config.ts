d89647e3cc6da6c04dd73445587862ae659d4bdb
wemsx <3551697397@qq.com>
2022-08-04 14:47:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/zelda-words/',
 
     assetsInclude: 'buffer',
   
